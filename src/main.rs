dc44958b5f01f8438ac6d2dc270a94bd15250568
Antonio Ruiz <ant.ruiz@gmail.com>
2018-11-05 22:17:58 +0100

    Refactor save image as ping in a function

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,20 @@ extern crate image;
 use std::fs::File;
 use std::path::Path;
 
-fn main() -> std::io::Result<()>
+fn write_image(filepath: &str, pixels: &[u8], x_size: usize, y_size: usize) -> std::io::Result<()>
 {
-    let image_path = Path::new("out/image.png");
+    let image_path = Path::new(filepath);
     let file = File::create(&image_path)?;
     let png_encoder = image::png::PNGEncoder::new(file);
+    png_encoder.encode(&pixels, x_size as u32, y_size as u32, image::ColorType::RGB(8))?;
+    Ok(())
+}
 
+fn main() -> std::io::Result<()>
+{
 	const NX : usize = 200;
 	const NY : usize = 100;
     let mut pixels : Vec<u8> = vec![0; NX * NY * 3];
-
 	for i in 0..NX {
 		for j in 0..NY {
 			let r  = i as f32 / NX as f32;
@@ -27,7 +31,7 @@ fn main() -> std::io::Result<()>
 		}
 	}
 
-    png_encoder.encode(&pixels, NX as u32, NY as u32, image::ColorType::RGB(8))?;
+    write_image("out/archivo.png", &pixels, NX, NY)?;;
 
     Ok(())
 }
